0b54d2bd7971d43575f8189c1368bb3dd2ef6ba6
TommyFrederiksen <Tommy.frederiksen@gmail.com>
2016-01-18 10:56:04 +0100

    fist draft
    first draft

---
+++ Figofy/OverviewViewController.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  Figofy
+//
+//  Created by Tommy on 31/12/2015.
+//  Copyright © 2015 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class OverviewViewController : UICollectionViewController{
+//    
+//    var overviews = [Overview]()
+//    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.overviews = [Overview(name: "Profil"),Overview(name: "Sø Kort Oversigt"),Overview(name: "Sø Liste Oversigt")]
+   }
+  override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    //override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     //   return self.overviews.count
+    //}
+    //override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    //{
+     //   let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)as UITableViewCell
+     //   var overview : Overview
+     //
+     //   overview = overviews[indexPath.row]
+     //
+     //   cell.textLabel?.text = overview.name
+     //
+     //   return cell
+     //
+    //}
+}
