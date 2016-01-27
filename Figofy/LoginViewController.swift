5acf55b5046b92261d8c7ccb8b917d546c2bf8c1
Mohammed Joseph Petrelli Salameh <Mulle@salameh.dk>
2016-01-27 10:58:28 +0100

    added gitignore

--- Figofy/LoginViewController.swift
+++ Figofy/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Figofy
 //
 //  Created by Tommy on 31/12/2015.
@@ -11,85 +11,77 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var backgroundView: UIView!
     
-    
-    @IBOutlet weak var usernameTextField: UITextField!
- 
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    let Users = [["Søren","Figofy"],["Kim","Figofy"],["Tommy","Figofy"]]
+//let Users = [["Søren","Figofy"],["Kim","Figofy"],["Tommy","Figofy"]]
     
     
     
+    
+    @IBOutlet weak var usernameTextField: TextFieldDesign!
+    
+    @IBOutlet weak var passwordTextField: TextFieldDesign!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
        
-        FigofyTextField(usernameTextField)
-        FigofyTextField(passwordTextField)
+//        FigofyTextField(usernameTextField)
+//        FigofyTextField(passwordTextField)
         
-    }
-    
-    @IBAction func Login(sender: AnyObject)
-    {
-        let username = usernameTextField.text;
-        let password = passwordTextField.text;
         
-        //empty fields in login
-        if(username == "" || password == "")
-        {
-            
-            let uiAlert = UIAlertController(title: "OBS", message: "Username & Password kan ikke være tomme", preferredStyle: UIAlertControllerStyle.Alert)
-            
-            uiAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action: UIAlertAction!) in self.navigationController?.popToRootViewControllerAnimated(true)}))
-            
-            self.presentViewController(uiAlert, animated: true, completion: nil)
-            
-            
-            
-        }
-        var indicator = false;
-        
-        for (name) in Users{
-            if (name.contains(username!))
-            {
-                if(name.contains(password!))
-                {
-                    indicator = true;
-                }
-                if(name.contains(password!) == false)
-                {
-                }
-            }
-            else
-            {
-            }
-        }
-        if(password == username)
-        {
-            indicator = false;
-        }
-        
-        if(indicator == false)
-        {
-            let uiAlert = UIAlertController(title: "OBS", message: "login fejlede", preferredStyle: UIAlertControllerStyle.Alert)
-            
-            uiAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action: UIAlertAction!) in self.navigationController?.popToRootViewControllerAnimated(true)}))
-            
-            self.presentViewController(uiAlert, animated: true, completion: nil)
-        }
-        
-    }
-    
-        
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+//    @IBAction func Login(sender: AnyObject)
+//    {
+//        let username = usernameTextField.text;
+//        let password = passwordTextField.text;
+//        
+//        //empty fields in login
+//        if(username == "" || password == "")
+//        {
+//            
+//            let uiAlert = UIAlertController(title: "OBS", message: "Username & Password kan ikke være tomme", preferredStyle: UIAlertControllerStyle.Alert)
+//            
+//            uiAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action: UIAlertAction!) in self.navigationController?.popToRootViewControllerAnimated(true)}))
+//            
+//            self.presentViewController(uiAlert, animated: true, completion: nil)
+//            
+//            
+//            
+//        }
+//        var indicator = false;
+//        
+//        for (name) in Users{
+//            if (name.contains(username!))
+//            {
+//                if(name.contains(password!))
+//                {
+//                    indicator = true;
+//                }
+//                if(name.contains(password!) == false)
+//                {
+//                }
+//            }
+//            else
+//            {
+//            }
+//        }
+//        if(password == username)
+//        {
+//            indicator = false;
+//        }
+//        
+//        if(indicator == false)
+//        {
+//            let uiAlert = UIAlertController(title: "OBS", message: "login fejlede", preferredStyle: UIAlertControllerStyle.Alert)
+//            
+//            uiAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action: UIAlertAction!) in self.navigationController?.popToRootViewControllerAnimated(true)}))
+//            
+//            self.presentViewController(uiAlert, animated: true, completion: nil)
+//        }
+        
+//    }
 
 }
 
