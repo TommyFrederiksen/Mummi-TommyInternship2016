7b08ee47e239a2ae6e1284a6b0912e4d4efc06fa
Mohammed Joseph Petrelli Salameh <Mulle@salameh.dk>
2016-01-27 10:50:31 +0100

    gitignore added, but empty

---
+++ Figofy/TextFieldDesign.swift
@@ -0,0 +1,9 @@
+//
+//  TextFieldDesign.swift
+//  Figofy
+//
+//  Created by Mohammed Joseph Petrelli Salameh on 27/01/16.
+//  Copyright © 2016 Tommy. All rights reserved.
+//
+
+import Foundation
---
+++ Figofy/TextFieldEffects.swift
@@ -0,0 +1,9 @@
+//
+//  TextFieldEffects.swift
+//  Figofy
+//
+//  Created by Mohammed Joseph Petrelli Salameh on 27/01/16.
+//  Copyright © 2016 Tommy. All rights reserved.
+//
+
+import Foundation
