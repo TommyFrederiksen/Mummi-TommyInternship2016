05ad5bfda5b95c8db56d18074eff8bf57b3853d6
Mohammed Joseph Petrelli Salameh <Mulle@salameh.dk>
2016-02-01 15:54:14 +0100

    working state of MAP currently only basic navigation and some Pin's

---
+++ Figofy/CustomFigofyAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  CustomFigofyAnnotation.swift
+//  Figofy
+//
+//  Created by Mohammed Joseph Petrelli Salameh on 01/02/16.
+//  Copyright © 2016 Tommy. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CustomFigofyAnnotation: MKPointAnnotation {
+    var pinCustomImageName: String!
+}
--- Figofy/MapViewViewController.swift
+++ Figofy/MapViewViewController.swift
@@ -7,22 +7,117 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import Contacts
 
-
-class MapViewViewController: UIViewController {
+class MapViewViewController: UIViewController, MKMapViewDelegate {
     
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+     //default value of the starting zoom
+    let regionRadius: CLLocationDistance = 1000
+    
+    let addresses = ["Kalkgravsvej 22, 4000 roskilde", "Fiskervejen 75,4000 Roskilde" , "Holmevej 41,3670 Veksø"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        map.delegate = self
+        
+        for add in addresses {
+            getPlacemarkFromAddress(add)
+        }
+        
+    
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
     }
     
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        return UITableViewCell()
+//    }
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        
+//    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
     }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        
+        map.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            centerMapOnLocation(loc)
+        }
+    }
+    
+    //calls before each annotation is placed
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        //for an IMAGE
+        //let reuseId = "figofy"
+        //var annoView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        //annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        //annoView!.image = UIImage(named: "PinLogo")
+        
+        if annotation.isKindOfClass(SeaAnnotation){
+            let annoView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Default")
+            annoView.animatesDrop = true
+            annoView
+            return annoView
+            
+        } else if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+        return nil
+    }
+    
+    func createAnnotationForLocation(location: CLLocation) {
+        let sea = SeaAnnotation(coordinate: location.coordinate)
+        
+        map.addAnnotation(sea)
+        
+    }
+    
+    func getPlacemarkFromAddress(address: String){
+        CLGeocoder().geocodeAddressString(address) { (placemarks: [CLPlacemark]?, error: NSError?) -> Void in
+            
+            if let marks = placemarks where marks.count > 0 {
+                if let loc = marks[0].location {
+                    //we have a location with coordinates
+                    self.createAnnotationForLocation(loc)
+                }
+            }
+        }
+    }
+
     
     
 }
